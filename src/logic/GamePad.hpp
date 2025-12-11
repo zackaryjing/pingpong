c75f668d34c0e28986418bb431c4bea5c59b9830
Jiang <108873506+zhiyangjing@users.noreply.github.com>
2025-12-12 00:27:59 +0800

    wip: changed some constant

--- src/logic/GamePad.hpp
+++ src/logic/GamePad.hpp
@@ -4,7 +4,7 @@ class GamePad {
 public:
     static constexpr int padWidth = 450;
     static constexpr int padHeight = 800;
-    static constexpr int margin = 200;
+    static constexpr int margin = 100;
     static constexpr int screenWidth = padWidth + 2 * margin;
     static constexpr int screenHeight = padHeight + 2 * margin;
 };
